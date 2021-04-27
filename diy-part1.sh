28f5b106d1107bbc81b30603c53a4b5bda5aa739
allok1985 <31798233+allok1985@users.noreply.github.com>
2021-04-28 01:07:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-rm -rf /package/lean/luci-app-n2n_v2
-rm -rf /package/lean/n2n_v2
+rm -rf ./package/lean/luci-app-n2n_v2
+rm -rf ./package/lean/n2n_v2
