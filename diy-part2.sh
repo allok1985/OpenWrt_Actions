1c906ad45f189c963f59ecd669d3f4543d0ef8d1
allok1985 <31798233+allok1985@users.noreply.github.com>
2021-05-13 15:42:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,24 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#修改默认的IP
+#sed -i 's/192.168.1.1/10.10.10.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#files目录权限
 #chmod -R 755 files
+
+#修改主机名
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Routes'' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#设置密码为空
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#版本号里显示一个自己的名字
+sed -i "s/OpenWrt /OpenWrt build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#修改插件名字
+#sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' openwrt/package/lean/luci-app-sfe/po/zh-cn/sfe.po
+        
+#修改上游DNS
+#sed -i "2i # network config" openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i "3i uci set network.lan.dns='10.10.10.2'"  openwrt/package/lean/default-settings/files/zzz-default-settings
