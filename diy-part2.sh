3071183be0f6a2d9caebfa07c747a44a169d2c1d
allok1985 <31798233+allok1985@users.noreply.github.com>
2023-09-06 22:07:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -56,17 +56,18 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-defaul
 #rm -rf ./feeds/kenzok8/luci-theme-tomato
 #rm -rf ./feeds/kenzok8/luci-theme-argon
 rm -rf ./feeds/luci/themes/luci-theme-argon
-#rm -rf ./package/luci-theme-argon
+rm -rf .package/feeds/luci/luci/Makefile/luci-theme-argon
 svn co https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 svn co https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+./scripts/feeds update package/luci-theme-argon
+./scripts/feeds install -a -f -p package/luci-theme-argon
+./scripts/feeds update package/luci-theme-argon-config
+./scripts/feeds install -a -f -p package/luci-theme-argon-config
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
-#if [ ! -d "./package/lean/luci-app-argon" ]; then git clone https://github.com/jerrykuku/luci-app-argon.git ./package/lean/luci-app-argon;   else cd ./package/lean/luci-app-argon; git stash; git stash drop; git pull; cd ..; cd ..; cd ..; fi;
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
+#if [ ! -d "./package/lean/luci-app-argon" ]; then git clone https://github.com/jerrykuku/luci-app-argon.git ./package/lean/luci-app-argon;   else cd ./package/lean/luci-app-argon; git stash; git stash drop; git pull; cd ..; cd ..; cd ..; fi;
 #if [ ! -d "./package/lean/luci-app-argon-config" ]; then git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config;   else cd ./package/lean/luci-app-argon-config; git stash; git stash drop; git pull; cd ..; cd ..; cd ..; fi;
-#./scripts/feeds update package/luci-theme-argon
-#./scripts/feeds install -a -f -p package/luci-theme-argon
-#./scripts/feeds update package/luci-theme-argon-config
-#./scripts/feeds install -a -f -p package/luci-theme-argon-config
+
 # 把bootstrap替换成argon为源码必选主题（可自行修改您要的,主题名称必须对,比如下面代码的[argon],源码内必须有该主题,要不然编译失败）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #sed -i 's/bootstrap/argon/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
