c5796af4f95862b19d38363a0bb21f8c575bdacd
allok1985 <31798233+allok1985@users.noreply.github.com>
2023-09-09 18:33:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -69,8 +69,8 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-defaul
 
 # 把bootstrap替换成argone为源码必选主题（可自行修改您要的,主题名称必须对,比如下面代码的[argone],源码内必须有该主题,要不然编译失败）
 sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/bootstrap/argone/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
+#sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci-nginx/Makefile
+#sed -i 's/bootstrap/argone/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 # 编译多主题时,设置固件默认主题（可自行修改您要的,主题名称必须对,比如下面代码的[argon],和肯定编译了该主题,要不然进不了后台）
 #sed -i "/exit 0/i\uci set luci.main.mediaurlbase='/luci-static/argon' && uci commit luci" "$FIN_PATH"
