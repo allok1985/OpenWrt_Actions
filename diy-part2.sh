424304514d4714e9c10bc0c3a1fb861d1b752a9f
allok1985 <31798233+allok1985@users.noreply.github.com>
2024-03-20 00:14:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -69,11 +69,11 @@ git clone -b 21.x https://github.com/sbwml/packages_lang_golang feeds/packages/l
 
 #https://github.com/kenzok8/small中xray-core
 # 移除 openwrt feeds 自带的核心包
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+#rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+#git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 # 更新 golang 1.22 版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 
 
