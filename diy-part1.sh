da34b1dc2826b06aa2433866c9dcd16733ad4175
allok1985 <31798233+allok1985@users.noreply.github.com>
2024-03-20 00:20:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,6 @@
 # 修改feeds源
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i "s/src-git/src-git-full/g" feeds.conf.default
-
 # 添加feeds源
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 
