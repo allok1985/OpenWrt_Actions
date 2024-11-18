f142ca2478ad586cef38d41ed7f71aa5ef03d938
allok1985 <31798233+allok1985@users.noreply.github.com>
2024-11-18 16:03:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -45,13 +45,13 @@ git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/l
 # ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-#kenzo 库冲突大佬处理2_https://github.com/kenzok8/small-package
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+#kenzo 库冲突大佬处理2_https://github.com/kenzok8/small-package  #20241118 miniupnpd报错
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb，miniupnpd}
 #kenzo 库syncthing报错 20241107换源
 #rm -rf feeds/smpackage/luci-app-syncthing
 #git clone https://github.com/Turing-China/luci-app-syncthing feeds/smpackage/luci-app-syncthing
 #20241118 miniupnpd报错
-rm -rf package/feeds/packages/miniupnpd
+#rm -rf package/feeds/packages/miniupnpd
 
 # 翻墙
 # sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
