62d44ed39fc5809db749d1f443d869ad6ad5b56c
allok1985 <31798233+allok1985@users.noreply.github.com>
2026-02-12 19:55:45 +0800

    Update x86_64-part2.sh

--- x86_64-part2.sh
+++ x86_64-part2.sh
@@ -94,9 +94,9 @@ rm -rf package/feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftn
 #git clone https://github.com/Turing-China/luci-app-syncthing feeds/smpackage/luci-app-syncthing
 # 20241120 miniupnpd报错
 # rm -rf package/feeds/packages/{miniupnpd,miniupnpd-iptables}
-# 20250318 xray-core报错
-rm -rf package/feeds/packages/xray-core
-rm -rf package/feeds/packages/geoview
+# #####################20250318 xray-core报错
+# rm -rf package/feeds/packages/xray-core
+# rm -rf package/feeds/packages/geoview
 
 #
 #https://github.com/kenzok8/small/issues/148#issuecomment-1999543382
